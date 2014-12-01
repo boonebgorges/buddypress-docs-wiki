720cb728463e16c3189ccb8a5371fc4c77d6eabe
Boone B Gorges <boonebgorges@gmail.com>
2014-12-01 10:37:33 -0500

    1.0.8 update.

--- loader.php
+++ loader.php
@@ -5,7 +5,7 @@ Description: Add a separate Wiki section to your site. Powered by BuddyPress Doc
 Author: Boone B Gorges
 Author URI: http://boone.gorg.es
 License: GPLv3
-Version: 1.0.7
+Version: 1.0.8
 */
 
 function bpdw_init() {
